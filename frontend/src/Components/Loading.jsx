a3b104b50a5cb3b0103bb4d3c3c3f8d92d458606
Raman <rushivenketesh@gmail.com>
2023-02-27 09:51:04 +0530

    done

---
+++ frontend/src/Components/Loading.jsx
@@ -0,0 +1,18 @@
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+
+function Loading() {
+  return (
+    <Box>
+      <Image
+        height="620px"
+        width="100%"
+        m="auto"
+        src="https://cdn.dribbble.com/users/90627/screenshots/1096260/loading.gif"
+        alt="loadingGifLink"
+      />
+    </Box>
+  );
+}
+
+export default Loading;
